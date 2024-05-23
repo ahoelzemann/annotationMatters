342c004605ec0f1d6043b8f347c33803d28f2b58
Alexander Hölzemann <alexander.hoelzemann@student.uni-siegen.de>
2024-05-23 15:33:25 +0200

    Update Dataset_BangleJS.py

--- deep_learning/dataloader/Datasets/Dataset_BangleJS.py
+++ deep_learning/dataloader/Datasets/Dataset_BangleJS.py
@@ -38,9 +38,9 @@ class Dataset(SensorDataset):
     def get_gui_label_timestamps(self, subject=None):
         import os
 
-        possible_labels = ['laying', 'sitting', 'walking', 'running', 'cycling', 'bus_driving', 'car_driving', 'cleaning', 'vacuum_cleaning', 'laundry', 
-                           'cooking', 'eating', 'shopping', 'showering', 'yoga', 'sport', 'playing_games', 'desk_work', 'guitar_playing', 'gardening', 'table_tennis', 
-                           'badminton', 'horse_riding', 'cleaning', 'reading', 'weightlifting', 'manual_work', 'dish_washing']
+        possible_labels = ['void', 'sitting', 'walking', 'running', 'car_driving', 'cycling', 'badminton', 'showering',
+                             'horse_riding', 'table_tennis', 'eating', 'gardening', 'playing_games', 'dish_washing',
+                              'cooking', 'cleaning', 'vacuum_cleaning', 'laundry', 'weightlifting', 'other']
         decompressed_folder = 'folder'
         raw_folder = 'folder'
         subfolders_weeks = ['week2']
